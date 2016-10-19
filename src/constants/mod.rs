8c0bea952f3758dbccb2f68f1d562af7dd3894fe
William Orr <will@worrbase.com>
2016-10-18 23:38:04 -0700

    Add platform specific constants

---
+++ src/constants/mod.rs
@@ -0,0 +1,20 @@
+
+#[cfg(target_os = "dragonfly")]
+mod dragonfly;
+#[cfg(target_os = "dragonfly")]
+pub use self::dragonfly::*;
+
+#[cfg(target_os = "freebsd")]
+mod freebsd;
+#[cfg(target_os = "freebsd")]
+pub use self::freebsd::*;
+
+#[cfg(target_os = "netbsd")]
+mod netbsd;
+#[cfg(target_os = "netbsd")]
+pub use self::netbsd::*;
+
+#[cfg(target_os = "openbsd")]
+mod openbsd;
+#[cfg(target_os = "openbsd")]
+pub use self::openbsd::*;
--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,45 @@
+#![no_std]
 #[allow(non_camel_case_types)]
-
+#[macro_use]
+extern crate bitflags;
 extern crate libc;
 
-use libc::{c_int, uintptr_t, c_short, c_ushort, c_uint, int64_t, c_void, timespec};
+#[allow(unused_imports)]
+use libc::{c_int, uintptr_t, c_short, c_ushort, c_uint, int64_t, intptr_t, uint32_t, c_void, size_t, timespec};
 
+pub mod constants;
+
+pub use self::constants::*;
+
+#[cfg(not(target_os="netbsd"))]
+#[derive(Clone, Copy, Debug)]
 #[repr(C)]
 pub struct kevent {
     ident: uintptr_t,
-    filter: c_short,
-    flags: c_ushort,
-    fflags: c_uint,
+    filter: EventFilter,
+    flags: EventFlag,
+    fflags: FilterFlag,
     data: int64_t,
     udata: *mut c_void,
 }
 
+#[cfg(target_os="netbsd")]
+#[derive(Clone, Copy, Debug)]
+#[repr(C)]
+pub struct kevent {
+    ident: uintptr_t,
+    filter: EventFilter,
+    flags: EventFlag,
+    fflags: FilterFlag,
+    data: int64_t,
+    udata: intptr_t,
+}
+
+#[allow(improper_ctypes)]
 extern "C" {
     pub fn kqueue() -> c_int;
+
+    #[cfg(not(target_os="netbsd"))]
     pub fn kevent(kq: c_int,
                   changelist: *const kevent,
                   nchanges: c_int,
@@ -23,4 +47,16 @@ extern "C" {
                   nevents: c_int,
                   timeout: *const timespec)
                   -> c_int;
+
+    #[cfg(target_os="netbsd")]
+    pub fn kevent(kq: c_int,
+                  changelist: *const kevent,
+                  nchanges: size_t,
+                  eventlist: *mut kevent,
+                  nevents: size_t,
+                  timeout: *const timespec)
+                  -> c_int;
+
+    #[cfg(target_os="netbsd")]
+    pub fn kqueue1(flags: c_int) -> c_int;
 }
